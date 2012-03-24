a93099f77f541a815c13daf2cc0c9dd5910c3a50
USUARIO <USUARIO@OMAR>
2012-03-24 18:09:08 -0500

    plantilla inicial eliminacion de mascotas

--- ejemplo.php
+++ ejemplo.php
@@ -30,9 +30,6 @@ function validarn(e)
 
 <style type="text/css">
 <!--
-body,td,th {
-	/* [disabled]color: #000; */
-}
 body {
 	background-color: #FFF;
 }
@@ -46,7 +43,7 @@ body {
 .Estilo18 {font-size: 12px; color: #FFFFFF; font-weight: bold; font-family: Verdana, Arial, Helvetica, sans-serif; }
 .Estilo19 {
 	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-style: italic;
+	font-style: normal;
 }
 -->
 </style></head>
@@ -60,53 +57,11 @@ body {
   </tr>
   <tr>
     <td height="90" colspan="2"><div align="center">
-      <p class="Estilo3 Estilo19">&nbsp;</p>
-      <p class="Estilo3 Estilo19">Zoonosis</p>
+      <p align="center" class="Estilo3 Estilo19"><strong>Zoonosis</strong></p>
     </div></td>
   </tr>
   <tr>
-    <td height="251" colspan="2"><form id="form1" name="form1" method="post" action="validar.php">
-        <table width="389" border="0" align="center" >
-          <tr>
-            <td width="383" height="226"><fieldset>
-              <legend class="Estilo18">Inicio de sesi&oacute;n</legend>
-              <br/>
-              <table width="348" border="0" align="center" bgcolor="#FFFFFF">
-                <tr>
-                  <td width="99" height="64"><div align="center"><img src="images/usuarioo.jpg" alt="usuario" width="70" height="70" /></div></td>
-                  <td width="239"><span class="Estilo14"> </span>
-                      <div align="left" ><span class="Estilo14">Usuario:
-                        <label> </label>
-                        </span>
-                          <label><br/>
-                          <input name="usuario" type="text"  maxlength="25" tabindex="1" />
-                          </label>
-                          <br/>
-                    </div></td>
-                
-                  <td height="64"><div align="center"><img src="images/llave.jpg" alt="llaveusuario" width="54" height="72" /></div></td>
-                  <td><span class="Estilo14">C&oacute;digo mascota:
-                      <label> </label>
-                    </span>
-                      <label><br/>
-                      <input name="codigo_animal" type="text" maxlength="16" tabindex="2" id="codigo_animal" />
-                    </label></td>
-                </tr>
-                <tr>
-                  <td height="21" colspan="2"><label></label>
-                      <div align="center">
-                        <label>
-                        <input type="submit" name="Submit" value="Iniciar Sesi&oacute;n" />
-                        </label>
-                    </div></td>
-                </tr>
-              </table>
-              <br/>
-            </fieldset></td>
-          </tr>
-        </table>
-        <br/>
-    </form></td>
+    <td height="251" colspan="2">&nbsp;</td>
   </tr>
 </table>
 </body>
