0e9fca3432f919568cff8765f646f8a36abbbe03
lleyzzon <lleyzzon@lleyzzon-PC>
2012-03-23 19:17:22 -0500

    Avance en la pagina FormularioRegistro en lo que concierne a dise?o y presentacion. Se adapta el estilo de las demas paginas ya implementadas.

--- FormularioRegistro.php
+++ FormularioRegistro.php
@@ -3,28 +3,117 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Documento sin título</title>
+<title>Zoonosis - Formulario de Registro</title>
+
+<script language="javascript">
+
+function validarh(e) 
+{
+	
+	tecla = (document.all) ? e.keyCode : e.which;
+    if (tecla==8) return true;
+    patron =/[-:\d\s]/;
+    te = String.fromCharCode(tecla);
+	letra=patron.test(te);
+    return (letra);
+}
+
+function validarn(e) 
+{
+	tecla = (document.all) ? e.keyCode : e.which;
+    if (tecla==8) return true;
+    patron = /\d/;
+    te = String.fromCharCode(tecla);
+    return patron.test(te);
+}
+//
+
+</script>
+
+<style type="text/css">
+<!--
+body,td,th {
+	/* [disabled]color: #000; */
+}
+body {
+	background-color: #FFF;
+}
+.Estilo3 {
+	font-weight: bold;
+	font-size: 28px;
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-style: italic;
+}
+.Estilo14 {font-family: Verdana, Arial, Helvetica, sans-serif; color: #333333; font-size: 12px;}
+.Estilo18 {font-size: 12px; color: #FFFFFF; font-weight: bold; font-family: Verdana, Arial, Helvetica, sans-serif; }
+.Estilo19 {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-style: italic;
+}
+-->
+</style>
+
 </head>
 
 <body>
 
-
-<form action="ResolverFormulario.php" method="get">
-  <p>Nombres
-    <input type="text" name="nombre" width="100px" id="nombre" /><br />
-  </p>
-  <p>Apellidos
-    <input type="text" name="apellido" width="100px" id="apellido" />
-  <br />
-  <label for="documento"><br />
-    CC</label>
-  <input name="documento" type="text" id="documento" />
-  <label for="correo"><br />
-    <br />
-    Correo Electronico</label>
-  <input type="text" name="correo" id="correo" />
-  </p>
-    
+    <br/><br/><table width="796" border="0" align="center" background="images/Bello Verde_800.jpg"   bgcolor="#FFFFFF" >
+    <tr>
+    <td width="790" height="139" colspan="2"><div align="center"><div>
+      <div align="right"></div>
+    </div><img src="images/images.jpg?q=tbn:ANd9GcSRJ_TeKNnn8Gn_EcTop6MJ0j57XOqV7kjY6Yd62StcrPukKOmc" alt="IMAGEN" width="152" height="108" align="middle"/></div></td>
+  </tr>
+    <tr>
+    <td height="90" colspan="2"><div align="center">
+      <p class="Estilo3 Estilo19">Registro de Usuarios </p>
+      <p class="Estilo3">Zoonosis</p>
+    </div></td>
+  </tr>
+    <tr>
+    <td height="251" colspan="2"><form id="form1" name="form1" method="post" action="validar.php">
+        <table width="389" border="0" align="center" >
+          <tr>
+             <form action="ResolverFormulario.php" method="get">
+                <p>Nombres
+                    <input type="text" name="nombre" width="100px" id="nombre" /><br />
+                </p>
+            <p>Apellidos
+                <input type="text" name="apellido" width="100px" id="apellido" />
+                <br />
+                <label for="documento"><br />
+                CC</label>
+                <input name="documento" type="text" id="documento" />
+                <label for="correo"><br />
+                 <br />
+                 Correo Electronico</label>
+                <input type="text" name="correo" id="correo" />
+            </p>
+            <p>
+                <label for="ciudad">Ciudad</label>
+                <input type="text" name="ciudad" id="ciudad" />
+            </p>
+            <p>
+                <label for="celu">Celular</label>
+                <input type="text" name="celu" id="celu" />
+                <label for="user"><br />
+            <br />
+                Nombre de Usuario</label>
+                <input type="text" name="user" id="user" />
+                <label for="password"><br />
+            <br />
+                Contraseña</label>
+            <input type="text" name="password" id="password" />
+            </p>
+            <p>
+            <input type="submit" value="Registrarse" />
+            </p>
+                            
+          </tr>
+        </table>
+        <br/>
+    </form></td>
+  </tr>
+</table>  
     
   <?php 
   
@@ -55,25 +144,6 @@ conexion($conexion);
 		echo ("No ha sido posible establecer la conexion");}
 ?>
 
-  <p>
-    <label for="ciudad">Ciudad</label>
-    <input type="text" name="ciudad" id="ciudad" />
-  </p>
-  <p>
-    <label for="celu">Celular</label>
-    <input type="text" name="celu" id="celu" />
-    <label for="user"><br />
-      <br />
-    Nombre de Usuario</label>
-    <input type="text" name="user" id="user" />
-    <label for="password"><br />
-      <br />
-    Contraseña</label>
-    <input type="text" name="password" id="password" />
-  </p>
-  <p>
-    <input type="submit" value="Registrarse" />
-  </p>
 </form>
 
 </body>
