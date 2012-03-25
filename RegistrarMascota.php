50fb9f972015536369082cf14db95d7e8c14a7cd
Alex <Alex@PC>
2012-03-25 16:39:55 -0500

    Se modificaron aspectos de presentacion

--- RegistrarMascota.php
+++ RegistrarMascota.php
@@ -29,6 +29,16 @@ function validarn(e)
 </script>
 
 <style type="text/css">
+body,td,th {
+	font-size: xx-small;
+}
+body {
+	background-image: url(images/Bello%20Verde_1200.jpg);
+	background-repeat: no-repeat;
+}
+a {
+	font-size: 9px;
+}
 <!--
 body,td,th {
 	/* [disabled]color: #000; */
@@ -51,9 +61,9 @@ body {
 -->
 </style></head>
 
-<body>
-<br/><br/><table width="785" border="0" align="center" background="images/Bello Verde_800.jpg"   bgcolor="#FFFFFF" >
-  
+<body bgproperties="fixed">
+
+ <br/><br/><table width="796" border="0" align="center"    bgcolor="" > 
   <tr>
     <td width="790" height="139" colspan="2"><div align="center">
         <div align="right"><a href="index.php" title="Buscar Mascota" target="_self">Cerrar Sesión</a></div><img src="images/images.jpg?q=tbn:ANd9GcSRJ_TeKNnn8Gn_EcTop6MJ0j57XOqV7kjY6Yd62StcrPukKOmc" alt="IMAGEN" width="152" height="108" align="middle"/></div></td>
@@ -71,7 +81,7 @@ body {
             <td width="647" height="226"><fieldset>
               <legend class="Estilo18">Buscar Mascotas<br/>
               </legend>
-              <table width="576" height="301" border="0" align="center" bgcolor="#FFFFFF">
+              <table width="576" height="301" border="0" align="center" bgcolor="">
                 <tr>
                   
                     
@@ -79,9 +89,8 @@ body {
                 
                   <td width="70" height="64"><div align="center"><img src="images/gato4.jpg" alt="usuario" width="70" height="70" /></div></td>
                   <td width="189"><span class="Estilo14"> </span>
-                    <div align="left" ><span class="Estilo14">Tipo de Animal:
-                      <label> </label>
-                      </span>
+                    <div align="left" ><span class="Estilo14"><strong>Tipo de Animal:
+                      </strong> </span>
                       <label><br/>
                         </label>
                       <label for="TipoAnimal"></label>
@@ -93,9 +102,8 @@ body {
                     </div></td>
                     <td height="64"><div align="center"><img src="images/perro3.jpg" alt="usuario" width="70" height="70" /></div></td>
                   <td><span class="Estilo14"> </span>
-                    <div align="left" ><span class="Estilo14">Ciudad de origen
-                        <label> </label>
-                      </span>
+                    <div align="left" ><span class="Estilo14"><strong>Ciudad de origen:
+                        </strong> </span>
                       <label><br/>
                       </label>
                       <label for="Raza2"></label>
@@ -104,11 +112,19 @@ body {
                     </div></td>
                 </tr>
                 <tr>
+                <td height="64"><div align="center"><img src="images/gato4.jpg" alt="usuario" width="70" height="70" /></div></td>
+                  <td><span class="Estilo14"> </span>
+                    <div align="left" ><span class="Estilo14"><strong>Sexo:</strong>
+                        </span>
+                      <label><br/>
+                      </label>
+                      <label for="Raza2"></label>
+                      <input name="Sexo" type="Text" maxlength="16" tabindex="2" id="Sexo" />
+                      <br/>
+                    </div></td>
                   <td height="64"><div align="center"><img src="images/gato4.jpg" alt="usuario" width="70" height="70" /></div></td>
                   <td><span class="Estilo14"> </span>
-                    <div align="left" ><span class="Estilo14">Raza
-                      <label> </label>
-                      </span>
+                    <div align="left" ><span class="Estilo14"><strong>Raza:</strong> </span>
                       <label><br/>
                       </label>
                       <label for="Raza2"></label>
@@ -117,31 +133,29 @@ body {
                     </div></td>
                     
                 
-                  <td height="64"><div align="center"><img src="images/perro3.jpg" alt="usuario" width="70" height="70" /></div></td>
-                  <td><span class="Estilo14"> </span>
-                    <div align="left" >Descripcion:<span class="Estilo14">
-                      <label> </label>
-                      </span>
-                      <label><br/>
-                      </label>
-                      <label for="Raza2"></label>
-                      <textarea name="Descripcion" cols="30" tabindex="2" id="Descripcion"></textarea>
-                      <br/>
-                    </div></td>
+                  
                     
                 </tr>
                 
                     
                   <td height="72"><div align="center"><img src="images/gato4.jpg" alt="usuario" width="70" height="70" /></div></td>
                   <td><span class="Estilo14"> </span>
-                    <div align="left" ><span class="Estilo14">Foto:
-                        <label> </label>
-                      </span>
+                    <div align="left" ><span class="Estilo14"><strong>Foto:</strong> </span>
                       <label><br/>
                       </label>
                       <label for="Raza2"></label>
                       <input name="foto" type="file" id="foto" size="23" />
 <br/>
+                    </div></td>
+                    
+                    <td height="64"><div align="center"><img src="images/perro3.jpg" alt="usuario" width="70" height="70" /></div></td>
+                  <td><span class="Estilo14"> </span>
+                    <div align="left" ><span class="Estilo14"><strong>Descripcion:</strong></span>
+                      <label><br/>
+                    </label>
+                      <label for="Raza2"></label>
+                      <textarea name="Descripcion" cols="30" tabindex="2" id="Descripcion"></textarea>
+                      <br/>
                     </div></td>
                 </tr>
                  
@@ -150,7 +164,7 @@ body {
                   <td height="21" colspan="5"><label></label>
                       <div align="center">
                         <label>
-                        <input type="submit" name="Generar registro" value="Generar Registro" id="Generar registro" />
+                        <input name="Generar registro" type="submit" class="Estilo14" id="Generar registro" value="Generar Registro" />
                         </label>
                     </div></td>
                 </tr>
