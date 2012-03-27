13a57d6989c4bf82935981286ce8440a5b49403f
pipex56527 <pipex56527@localhost>
2012-03-26 21:22:57 -0500

    Se modifica el script conexionMySQL.php para mejorar funcionalidad.

--- conexionMySQL.php
+++ conexionMySQL.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Documento sin título</title>
-</head>
-
-<body>
-<?php 
-
-
-//Funcion utilizada para conectarse a la Base de Datos
-
-function conexion($conex){
-
-	$conex = mysql_connect("localhost", "root", "92120815");
-	mysql_select_db('prueba');
+<?php
+function Conectarse()
+{
+if (!($link=mysql_connect("localhost","root","")))
+{
+echo "No se pudo hacer conexion a la base de datos.";
+exit();
+}
+if (!mysql_select_db("zoonosis",$link))
+{
+exit();
+}
+return $link;
 }
 ?>
-</body>
-</html>
