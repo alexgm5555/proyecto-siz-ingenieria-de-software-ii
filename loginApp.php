61ea89cff19d596f9f5f5714403a30c0f74e87f0
pipex56527 <pipex56527@localhost>
2012-03-26 21:21:08 -0500

    Se agrega el script loginApp.php  que valida el inicio de sesion de un usuario.

---
+++ loginApp.php
@@ -0,0 +1,16 @@
+<?php
+include ("conexionMySQL.php");
+$conexion = Conectarse();
+$usuario=$_POST['usuario'];
+$contrasena=$_POST['contrasena'];
+$consulta2 = "SELECT * from persona where usuario='$usuario' and contrasena='$contrasena'";
+$resultado2 = mysql_query($consulta2, $conexion) or die(mysql_error());
+$numfilas2 = mysql_num_rows($resultado2);
+if($numfilas2 !=0){
+	echo "La validacion del usuario se hizo satisfactoriamente. <br>Por favor quien este encargado de la pagina que se enlaza con login satisfactorio en bombas!! <br>la necesito para ponerla en el header y asi configurar la sesion ... :)";
+	//header ('location:BuscarMascota.php'); 
+}
+else{
+	header ('location:login.php?LoginMesagge=1'); 
+}
+?>
