a3b99669b3e6a55fe8f2f5a8eaf84f8f9255a63d
pipex56527 <pipex56527@localhost>
2012-04-14 10:51:26 -0500

    Validaciones al logueo...

--- loginApp.php
+++ loginApp.php
@@ -3,7 +3,7 @@ include ("conexionMySQL.php");
 $conexion = Conectarse();
 $usuario=$_POST['usuario'];
 $contrasena=$_POST['contrasena'];
-$consulta2 = "SELECT * from persona where usuario='$usuario' and contrasena='$contrasena'";
+$consulta2 = "SELECT * from usuarios where UserName='$usuario' and Password='$contrasena'";
 $resultado2 = mysql_query($consulta2, $conexion) or die(mysql_error());
 $numfilas2 = mysql_num_rows($resultado2);
 if($numfilas2 !=0){
