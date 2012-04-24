ddd6486dc3a0c1b6411970d72de25f3a4c961bb5
Alex <Alex@PC>
2012-04-24 17:15:24 -0500

    Se Agrego EliminarRegistroUsuario,se mejoro la funcionalidad de MascotasRegistraadas

---
+++ ClasificadorRoles.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+//error_reporting("E_PARSE");
+//Validamos si la sesión ya fue creada:
+session_start();
+if ( isset( $_SESSION['usuario'])) {
+
+//Permite continuar en la página
+}
+else{	
+	header ('location:index.php?LoginMesagge=2'); 
+	} 
+
+?>
+<!--
+	Website Name by Adonis Ronquillo for Free Website Templates
+	www.freewebsitetemplat.es / www.doni.us
+	Images by Image Base http://imagebase.davidniblack.com/
+	Released under the Creative Commons Attribution 3.0 License.
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<title>Zoonisis</title>
+		<link href="http://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<div id="outer">
+			<div id="header">
+				<div id="logo">
+					<h1> Zoonosis</h1>
+				</div>
+<div id="nav">
+			  		
+                    <ul>
+						<li class="first active"><a href="index.php">Inicio</a></li>			
+						<li><a href="#">Acerca de</a></li>
+                       	<li class="last"><a href="#">Contacto</a></li>
+						<li class="last"><a href="Logout.php">Cerrar Sesión</a></li>							
+					</ul>
+                    
+              <div align="right">Usuario Activo: <?php echo $_SESSION['usuario'];?></div>
+</div>
+			</div>
+			<div id="banner">
+				<img src="images/pic01.jpg" width="1120" height="240" alt="" />
+			</div>
+			<div id="main">
+				<div id="content">
+				
+					<div id="box1">
+						<h2>Bienvenid@ a Zoonisis <?php echo $_SESSION['usuario'];?></h2>
+						<img class="left round" src="images/pic02.jpg" width="200" height="180" alt=""  />En esta página podrás encontrar las estadísticas generales del Sistema de Información Zoonisis.
+					</div>
+					
+					<div id="box2">
+						<h2>En Zoonisis ellos son nuestros amigos</h2>
+						<img class="left round" src="images/pic03.jpg" width="200" height="180" alt="" />En muchas ocasiones nos preguntamos ¿dónde están nuestros amigos cuando los necesitamos?
+					y la respuesta es muy sencilla, están por todo el planeta. En Zoonisis los animales son nuestros amigos, y como amigos nuestro objetivo es brindarles apoyo para todas las necesidades que ellos tengan.</div>
+					
+					<br class="clear" />
+				</div>
+				<div id="sidebar">
+					<h3>
+                    
+                  </h3>
+					<div class="form">
+					  <p><?php echo $_SESSION['usuario'];?>, puedes realizar las siguientes actividades</p>
+                      <ol>
+                        <li><a href="RegistrarMascota.php">Registrar Mascotas</a></li>
+                        <li><a href="BuscarMascota.php">Buscar Mascotas</a></li>
+                        <li><a href="MascotasRegistradas.php">Administrar Animales Registrados</a></li>
+                        <li>Módulo</li>
+                        <li>Módulo</li>
+                        <li><a href="AdministracionUsuarios.php">Administracion de Usuarios</li>
+                        <li><a href="AdministracionSedes.php">Administración de Sedes</a></li>
+                        <li><a href="Estadisticas.php">Estadísticas</a></li>
+                      </ol>
+                  </div>
+				  <h3>Enlaces de Interés</h3>
+					<ul class="linkedList">
+						<li class="first">
+							<a href="http://www.freewebtemplates.com/free-templates/">Templates Gratis</a>
+						</li>
+						<li>
+							<a href="http://www.google.com.co">Google</a>
+						</li>
+						<li>
+							<a href="http://docs.google.com">Google Docs</a>
+						</li>
+					</ul>
+				  <p>&nbsp;</p>
+            </div>
+				<br class="clear" />
+			</div>
+		</div>
+		<div style="margin: 1em 0 3em 0; text-align: center;">
+        Este Sitio Web es desarrollado en la Universidad Nacional de Colombia
+			<br />Diseñado y Provisto por Zoonisis Team
+			<br />© 2012.</div>
+</body>
+</html>
