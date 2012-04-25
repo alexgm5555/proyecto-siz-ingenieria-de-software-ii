5f960a64c131c36e66591cdf5e40e8d6f320206c
Alex <Alex@PC>
2012-04-24 20:28:44 -0500

    Uni?n con lo de Duban

---
+++ EliminarUsuario.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Este bloque de PHP nos permite guardar en la Base de Datos las variables solicitadas 
+//al usuario en FormularioRegistro.php
+
+echo "Duvan";
+function EncontrarUsuario(){
+
+$conexion = true;
+$UsuarioEliminar = $_POST['UsuarioparaEliminar'];
+
+Conectarse($conexion);
+
+if($conexion){
+    $query = "select Nombres, Apellidos, Cedula, Ciudad  from zoonosis.Usuarios 
+                where (Cedula = 0 and TipoUsuario='Particular')";
+    $consulta = mysql_query($query) or die(mysql_error());
+    
+}
+return $consulta;
+}
+?>
