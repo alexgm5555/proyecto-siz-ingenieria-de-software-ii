b3ad1280b1ece7e651ac79553772dab87d1e3f16
Duvan <Duvan@Duvan-PC>
2012-04-24 19:26:12 -0500

    Agregada funcionalidad a Administraci?n de Empleados

--- AdministracionUsuarios.php
+++ AdministracionUsuarios.php
@@ -130,7 +130,7 @@ $numfilas = mysql_num_rows($resultado);
                                         $consulta = mysql_query("select * from zoonosis.ciudades order by Nombre_Ciudad ASC");
                                         echo "<select name = 'CiudadUsuario' id = 'CiudadUsuario'>";
                                         while($fila = mysql_fetch_array($consulta)){
-                                            echo "<option value='".$fila[1]."'>".utf8_encode($fila[1])."</option>";
+                                            echo "<option value='".$fila[1]."'>".($fila[1])."</option>";
                                         }
                                     echo "</select>";
                                     }
