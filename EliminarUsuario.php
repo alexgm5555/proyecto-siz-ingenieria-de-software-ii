1d7c99bad50e40b5aaf233a9548a62c944613127
lleyzzon <lleyzzon@lleyzzon-PC>
2012-04-25 23:34:27 -0500

    Se realizan las siguientes modificaciones: -Se hacen modificaciones en los enlaces del iframe de modificar usuario

--- EliminarUsuario.php
+++ EliminarUsuario.php
@@ -1,14 +1,4 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-//Este bloque de PHP nos permite guardar en la Base de Datos las variables solicitadas 
-//al usuario en FormularioRegistro.php
-
-echo "Duvan";
 function EncontrarUsuario(){
 
 $conexion = true;
@@ -18,8 +8,9 @@ Conectarse($conexion);
 
 if($conexion){
     $query = "select Nombres, Apellidos, Cedula, Ciudad  from zoonosis.Usuarios 
-                where (Cedula = 0 and TipoUsuario='Particular')";
+                where (Cedula = $UsuarioEliminar and TipoUsuario='Particular')";
     $consulta = mysql_query($query) or die(mysql_error());
+    echo $consulta;
     
 }
 return $consulta;
