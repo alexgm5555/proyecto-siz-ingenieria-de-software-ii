755eaae2a0dfe0b077619d8fa79ed8e4a96b265c
lleyzzon <lleyzzon@lleyzzon-PC>
2012-05-06 01:27:00 -0500

    Se realizan las siguientes operaciones: - Se identan corrrectamente algunas paginas.

--- ModificarUsuario.php
+++ ModificarUsuario.php
@@ -1,7 +1,18 @@
-<?php
+<html>
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+    <head>
+
+        <link rel="stylesheet" type="text/css" href="style2.css" />
+
+    </head>
+        <body>
+            <div id="box1">
+						<h2>Bienvenid@ a Zoonosis</h2>
+						<p>
+							En Zoonosis nos preocupamos por el estado y los derechos de los animales, por ello nos dedicamos a desarrollar actividades que procuren por su bienestar. Si quieres se parte de Zoonosis Colombia regístrate y forma parte de nuestra comunidad.
+                                                </p>
+					</div>
+                               
+            <p>&nbsp;</p>
+        </body>
+</html>
