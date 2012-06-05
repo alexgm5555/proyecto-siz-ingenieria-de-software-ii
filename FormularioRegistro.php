e1ce8697901a517b228658af58394fbfc5932463
pipex56527 <pipex56527@localhost>
2012-06-05 12:16:53 -0500

    Se agregan restricciones html a los campos de index y FormularioRegistro.php

--- FormularioRegistro.php
+++ FormularioRegistro.php
@@ -6,6 +6,84 @@ error_reporting("E_PARSE");
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
+    
+    <!--
+    Script que permite capturar la tecla enter y enviar el formulario
+    -->
+    <script language="javascript "type="text/javascript">
+		function stopRKey(evt) { 
+		var evt = (evt) ? evt : ((event) ? event : null);
+		var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+		if ((evt.keyCode == 13) && (node.type=="text")) {return false;}
+		}
+		document.onkeypress = stopRKey; 
+	</script>
+    
+    <script language="javascript">
+
+	function valida_envia(){ 
+	var usuarioc = document.loginform.usuario.value;
+	var contrasenac = document.loginform.contrasena.value;
+	var patron = /'/;
+	var patron2 = /"/;
+	
+	//valido el usuario
+	
+	// El usuario no puede ser vacio:
+    if (document.loginform.usuario.value.length==0){ 
+       alert("Ingrese un nombre de Usuario") 
+       document.loginform.usuario.focus() 
+       return 0; 
+    } 
+	
+	// La contraseña no puede ser vacia:
+    else if (document.loginform.contrasena.value.length==0){ 
+       alert("La contraseña no puede ser vacía.") 
+       document.loginform.contrasena.focus() 
+       return 0; 
+    } 
+	
+	//El usuario no puede ser mayor que 20 caracteres
+	else if (document.loginform.usuario.value.length > 40){ 
+       alert("El usuario no puede contener más de 40 caracteres.") 
+       document.loginform.usuario.focus() 
+       return 0; 
+    }
+	
+	//La contraseña no puede ser mayor que 20 caracteres
+	else if (document.loginform.contrasena.value.length > 40){ 
+       alert("La contraseña no puede contener más de 40 caracteres.") 
+       document.loginform.contrasena.focus() 
+       return 0; 
+    } 
+	
+	//El usuario no puede contener comillas ni simples ni normales
+	else if (usuarioc.search(patron) != -1 || usuarioc.search(patron2) != -1){ 
+       alert("El Usuario no puede llevar comillas.") 
+       document.loginform.usuario.focus() 
+       return 0; 
+    } 
+	
+	//La contraseña no puede contener comillas ni simples ni normales
+	else if (contrasenac.search(patron) != -1 || contrasenac.search(patron2) != -1){ 
+       alert("La Contraseña no puede llevar comillas.") 
+       document.loginform.contrasena.focus() 
+       return 0; 
+    } 
+	
+	//el formulario se envia 
+   
+    document.loginform.submit(); 
+} 
+
+// Funcion que llama a la anterior si en alguno de los campos se presiona la tecla enter
+	function onEnter(ev) {  
+	if(ev==13)    { 
+	valida_envia();  
+	}  
+	}
+	
+	</script>
 
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
@@ -51,15 +129,15 @@ error_reporting("E_PARSE");
                                         <tr>
                                             <td>Nombres:</td>
                                             <td><label for="NombresUsuario"></label>
-                                            <input name="NombresUsuario" type="text" id="NombresUsuario" size="45px" /></td>
+                                            <input name="NombresUsuario" type="text" id="NombresUsuario" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td>Apellidos:</td>
-                                            <td><input name="ApellidosUsuario" type="text" id="ApellidosUsuario" size="45px" /></td>
+                                            <td><input name="ApellidosUsuario" type="text" id="ApellidosUsuario" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td>Documento de Identidad:</td>
-                                            <td><input name="DocumentoUsuario" type="text" id="DocumentoUsuario" size="45px" /></td>
+                                            <td><input name="DocumentoUsuario" type="text" id="DocumentoUsuario" size="45px" maxlength="15" /></td>
                                         </tr>
                                         <tr>
                                             <td>Ciudad:</td>
@@ -85,27 +163,27 @@ error_reporting("E_PARSE");
                                         </tr>
                                         <tr>
                                             <td>E-mail:</td>
-                                            <td><input name="emailUsuario" type="text" id="emailUsuario" size="45px" /></td>
+                                            <td><input name="emailUsuario" type="text" id="emailUsuario" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td>Teléfono:</td>
-                                            <td><input name="telefonoUsuario" type="text" id="telefonoUsuario" size="45px" /></td>
+                                            <td><input name="telefonoUsuario" type="text" id="telefonoUsuario" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td>Celular:</td>
-                                            <td><input name="CelularUsuario" type="text" id="CelularUsuario" size="45px" /></td>
+                                            <td><input name="CelularUsuario" type="text" id="CelularUsuario" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td>Nombre de Usuario:</td>
-                                            <td><input name="UserName" type="text" id="UserName" size="45px" /></td>
+                                            <td><input name="UserName" type="text" id="UserName" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td>Contraseña:</td>
-                                            <td><input name="Password" type="password" id="Password" size="45px" /></td>
+                                            <td><input name="Password" type="password" id="Password" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td>Confirmar Contraseña:</td>
-                                            <td><input name="confPassword" type="password" id="confPassword" size="45px" /></td>
+                                            <td><input name="confPassword" type="password" id="confPassword" size="45px" maxlength="40" /></td>
                                         </tr>
                                         <tr>
                                             <td colspan="2" >
