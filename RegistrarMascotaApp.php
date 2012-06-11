910fd90be83a7b2c0bb31f1e0166cc72b361d526
Alex <Alex@PC>
2012-06-10 23:30:01 -0500

    coloque seguridad en la insercion del archivo  solo funciona en el del usuario el empleado aun le hace falta

--- RegistrarMascotaApp.php
+++ RegistrarMascotaApp.php
@@ -16,28 +16,42 @@ $TipoAnimal=$_POST['Tipo'];
 $EstadoAnimal=$_POST['EstadoAnimal'];
 $FotoAnimal1=$_FILES["FotoAnimal"]["name"];
 $FotoAnimal2=$_FILES["FotoAnimal"]["tmp_name"];
+$ExtFotoanimal=pathinfo($FotoAnimal1);
 
-#$FotoAnimal= "images/".$FotoAnimal1;/*le adiciona el nombre de la carpeta al nombre de la imagen*/
-#copy($FotoAnimal2,$FotoAnimal);/*codigo para copiar el archivo temporal al destino que tiene especifico*/
-		
-$conexion=true;
-Conectarse($conexion);
+//condicion para no dejar pasar un archivo diferente a un jpg
 
-$FotoAnimal= "images/".$FotoAnimal1;/*le adiciona el nombre de la carpeta al nombre de la imagen*/
-copy($FotoAnimal2,$FotoAnimal);/*codigo para copiar el archivo temporal al destino que tiene especifico*/
+if(is_uploaded_file($FotoAnimal2)){
+    if(($ExtFotoanimal['extension']=="jpg")||($ExtFotoanimal['extension']=="JPG")||($ExtFotoanimal['extension']=="PNG")||($ExtFotoanimal['extension']=="png")){
+        
+        
+    
+        #$FotoAnimal= "images/".$FotoAnimal1;/*le adiciona el nombre de la carpeta al nombre de la imagen*/
+        #copy($FotoAnimal2,$FotoAnimal);/*codigo para copiar el archivo temporal al destino que tiene especifico*/
 
-if($conexion){
-		$fa=mysql_query("select * from usuarios where UserName = '$idUsuario'");
-		$faa=mysql_fetch_array($fa);
-		$Cedula= $faa['Cedula'];
-                $fat=mysql_query("select * from tipos_animal where idTipos_Animal = '$TipoAnimal'");
-                $fatt=mysql_fetch_array($fat);
-                $TipoAnimal=$fatt['Tipo'];
-		
-	$query = ("insert into animal values (null, 'Particular','$Cedula','$TipoAnimal','En Adopcion','$NombreAnimal','$SexoAnimal','$RazaAnimal','$ColorAnimal','$TamañoAnimal','$EdadAnimal','$PesoAnimal','$FotoAnimal','','$HabilidadAnimal')");/*inserta los valores en la BD*/
-	
-	mysql_query($query)or die(mysql_error());
-	header ('location:MascotasRegistradas.php?Message=3');
+        $conexion=true;
+        Conectarse($conexion);
+
+        $FotoAnimal= "images/".$FotoAnimal1;/*le adiciona el nombre de la carpeta al nombre de la imagen*/
+        copy($FotoAnimal2,$FotoAnimal);/*codigo para copiar el archivo temporal al destino que tiene especifico*/
+
+        if($conexion){
+                        $fa=mysql_query("select * from usuarios where UserName = '$idUsuario'");
+                        $faa=mysql_fetch_array($fa);
+                        $Cedula= $faa['Cedula'];
+                        $fat=mysql_query("select * from tipos_animal where idTipos_Animal = '$TipoAnimal'");
+                        $fatt=mysql_fetch_array($fat);
+                        $TipoAnimal=$fatt['Tipo'];
+
+                $query = ("insert into animal values (null, 'Particular','$Cedula','$TipoAnimal','En Adopcion','$NombreAnimal','$SexoAnimal','$RazaAnimal','$ColorAnimal','$TamañoAnimal','$EdadAnimal','$PesoAnimal','$FotoAnimal','','$HabilidadAnimal')");/*inserta los valores en la BD*/
+
+                mysql_query($query)or die(mysql_error());
+                header ('location:MascotasRegistradas.php?Message=3');
+                }
+
+        }
+else{
+    header ('location:RegistrarMascota.php?Message=1');
+}
 	}
 
 ?>
