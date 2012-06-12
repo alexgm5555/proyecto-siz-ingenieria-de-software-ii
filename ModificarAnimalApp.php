d1ce37a7feeb2e8f5f681b598cdd7f89f29a0ba4
Duvan <Duvan@Duvan-PC>
2012-06-12 15:44:15 -0500

    Captura de error de la BD y correccion de las validaciones en registrar y modificar mascota.

---
+++ ModificarAnimalApp.php
@@ -0,0 +1,45 @@
+<?php include ("conexionMySQL.php")?>
+<?php
+
+
+
+$NombreAnimal=$_POST['NombreAnimal'];
+$RazaAnimal=$_POST['RazaAnimal'];
+$SexoAnimal=$_POST['SexoAnimal'];
+$EdadAnimal=$_POST['EdadAnimal'];
+$ColorAnimal=$_POST['ColorAnimal'];
+$TamañoAnimal=$_POST['TamañoAnimal'];
+$PesoAnimal=$_POST['PesoAnimal'];
+$HabilidadAnimal=$_POST['HabilidadAnimal'];
+$idAnimal=$_POST['idAnimal'];
+#$EstadoAnimal=$_POST['EstadoAnimal'];
+$FotoAnimal1=$_FILES["FotoAnimal"]["name"];
+$FotoAnimal2=$_FILES["FotoAnimal"]["tmp_name"];
+
+		
+$conexion=true;
+Conectarse($conexion);
+
+
+if($conexion){
+	if($FotoAnimal1){
+		$FotoAnimal= "images/".$FotoAnimal1;/*le adiciona el nombre de la carpeta al nombre de la imagen*/
+		copy($FotoAnimal2,$FotoAnimal);/*codigo para copiar el archivo temporal al destino que tiene especifico*/	
+		$query=("update animal set Nombre = '$NombreAnimal',Raza = '$RazaAnimal',Edad = '$EdadAnimal',Color = '$ColorAnimal',Tamaño = '$TamañoAnimal',Peso = '$PesoAnimal',Habilidad = '$HabilidadAnimal',Foto = '$FotoAnimal' where idAnimal = '$idAnimal'");
+	}
+	else {
+		$query=("update animal set Nombre = '$NombreAnimal',Raza = '$RazaAnimal',Edad = '$EdadAnimal',Color = '$ColorAnimal',Tamaño = '$TamañoAnimal',Peso = '$PesoAnimal',Habilidad = '$HabilidadAnimal' where idAnimal = '$idAnimal'");
+		}
+		
+	# Raza = '$RazaAnimal',Sexo = '$SexoAnimal',,Color = '$ColorAnimal',Tamaño = '$TamañoAnimal',Peso = '$PesoAnimal'Habilidad = '$HabilidadAnimal',
+	
+        $ErrorBase = "<script> alert('Ha ocurrido un error durante la actualizacion, por favor revise los datos.')
+                                location.href='/proyecto-siz-ingenieria-de-software-ii/MascotasRegistradas.php';
+                                </script>
+                             ";
+                
+        mysql_query($query)or die($ErrorBase);
+	header ('location:MascotasRegistradas.php?Message=2');
+	}
+
+?>
